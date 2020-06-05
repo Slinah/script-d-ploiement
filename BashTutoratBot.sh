8a1be5be8928c55341cb1dfda7bb66b9a1ea68db
Léo <leo.catifait@epsi.fr>
2020-06-05 16:08:48 +0200

    try

--- BashTutoratBot.sh
+++ BashTutoratBot.sh
@@ -7,7 +7,7 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 if [ "$(id -u)" != "0" ]; then
-  echo -e "${RED}Tu n'es pas en root :'(${NC}"
+  echo "${RED}Tu n'es pas en root :'(${NC}"
   exit 1
 fi
 while :
@@ -28,9 +28,9 @@ read chx_menu
 stty echo
 
 if [ $chx_menu = 1 ]; then # test si le numéro 1 est sélectionner.
-  echo -e "${BLUE}1- Update/Upgrade & installation des paquets.${NC}" 
-  echo -e "${GREEN}Tu as Debian !${NC}"
-  echo -e "${GREEN}Programme d'installation de Debian :)${NC}"
+  echo "${BLUE}1- Update/Upgrade & installation des paquets.${NC}" 
+  echo "${GREEN}Tu as Debian !${NC}"
+  echo "${GREEN}Programme d'installation de Debian :)${NC}"
   debxport=$(grep "export maccent" /etc/bash.bashrc | cut -c8-14 | head -n 1)
   #stocke le résultat de la première ligne et des caractères 8 à 14 pour la recherche
   #"export maccent" du fichier bash.bashrc dans la variable debxport
@@ -44,18 +44,18 @@ if [ $chx_menu = 1 ]; then # test si le numéro 1 est sélectionner.
   deb-src http://deb.debian.org/debian/ stable main contrib non-free
   deb http://deb.debian.org/debian/ stable-updates main contrib non-free
   deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free" > sources.list
-  echo -e "${GREEN}Les sources ont été modifiées (t'es maintenant sûr de pouvoir installer).${NC}"
+  echo "${GREEN}Les sources ont été modifiées (t'es maintenant sûr de pouvoir installer).${NC}"
   #Update upgrade des sources
   apt-get update -y --force-yes #update/upgrade
   apt-get upgrade -y --force-yes
-  echo -e "${GREEN}Update/Upgrade effectués${NC}"
-  echo -e "${GREEN}Installation de Openssh, et MariaDB ${NC}"
+  echo "${GREEN}Update/Upgrade effectués${NC}"
+  echo "${GREEN}Installation de Openssh, et MariaDB ${NC}"
   #Installation du paquet SSH
   apt-get install openssh-server -y --force-yes
-  echo -e "${GREEN}Paquet SSH installé.${NC}"
+  echo "${GREEN}Paquet SSH installé.${NC}"
   #Installation du paquet FTP
   apt-get install vsftpd -y --force-yes
-  echo -e "${GREEN}Paquet FTP installé.${NC}"
+  echo "${GREEN}Paquet FTP installé.${NC}"
   cd /etc
   if [ -d "/etc/vsftpd.conf" ];then
     rm /etc/vsftpd.conf
@@ -81,73 +81,72 @@ if [ $chx_menu = 1 ]; then # test si le numéro 1 est sélectionner.
   service restart vsftpd
   #Installation du paquet MariaDB
   apt-get install mariadb-server -y --force-yes
-  echo -e "${GREEN}Paquet MariaDB installé.${NC}"
+  echo "${GREEN}Paquet MariaDB installé.${NC}"
     if [ -d "/home/$userdeladebian/.ssh/" ];then #si .ssh/ est deja créer, ne rien faire
     command > /dev/null 2>&1
-    echo -e "${GREEN}Répertoire .ssh/ déjà crée.${NC}"
+    echo "${GREEN}Répertoire .ssh/ déjà crée.${NC}"
   else #si .ssh/ n'est pas créer, le créer
     su -l $userdeladebian -c "mkdir .ssh/"
-    echo -e "${GREEN}Répertoire .ssh/ crée.${NC}"
+    echo "${GREEN}Répertoire .ssh/ crée.${NC}"
   fi
   cd .ssh/
   if [ -d "/home/$userdeladebian/.ssh/authorized_keys" ];then #si authorized_keys est deja créer, ne rien faire
     command > /dev/null 2>&1
-    echo -e "${GREEN}Fichier authorized_keys déjà crée.${NC}"
+    echo "${GREEN}Fichier authorized_keys déjà crée.${NC}"
   else #si authorized_keys n'est pas créer, le créer
     touch authorized_keys
-    echo -e "${GREEN}Fichier authorized_keys crée.${NC}"
+    echo "${GREEN}Fichier authorized_keys crée.${NC}"
   fi
   chmod 755 -R /home/$userdeladebian/.ssh/ #attribution des droits 755 a .ssh/
 elif [ $chx_menu = 2 ]; then # test si le numéro 2 est sélectionner.
-  echo -e "${BLUE}2- Création d'utilisateurs.${NC}"
+  echo "${BLUE}2- Création d'utilisateurs.${NC}"
   if grep -i "adminweb" /etc/passwd;then #test pour voir si l'user existe deja
     userweb=1
-    echo -e "${BLUE}L'utilisateur est déjà présent.${NC}"
+    echo "${BLUE}L'utilisateur est déjà présent.${NC}"
   else
     userweb=0
-    echo -e "${RED}L'utilisateur n'a pas été créer, vous allez le créer.${NC}"
+    echo "${RED}L'utilisateur n'a pas été créer, vous allez le créer.${NC}"
   fi
   if [ $userweb == 0 ]; then
     adduser adminweb #création de l'utilisateur
-    echo -e "${GREEN}Utilisateur adminweb crée.${NC}"
+    echo "${GREEN}Utilisateur adminweb crée.${NC}"
   fi
   unset userweb
-  ese
   if grep -i "adminbdd" /etc/passwd;then
     userbdd=1
-    echo -e "${BLUE}L'utilisateur est déjà présent.${NC}"
+    echo "${BLUE}L'utilisateur est déjà présent.${NC}"
   else
     userbdd=0
-    echo -e "${RED}L'utilisateur n'a pas été créer, vous allez le créer.${NC}"
+    echo "${RED}L'utilisateur n'a pas été créer, vous allez le créer.${NC}"
   fi
   if [ $userbdd == 0 ]; then
     adduser adminbdd
     passwd adminbdd
-    echo -e "${GREEN}Utilisateur adminbdd crée.${NC}"
+    echo "${GREEN}Utilisateur adminbdd crée.${NC}"
   fi
   unset userbdd
 elif [ $chx_menu = 3 ]; then # test si le numéro 3 est sélectionner.
-  echo -e "${BLUE}3- SSH.${NC}"
-    chown -R $userdeladebian /home/$userdeladebian/.ssh/ #Attribution du dossier .ssh/ a l'user
-    su -l $userdeladebian -c "ssh-keygen -t rsa" #génération des Clefs
-        echo -e "${GREEN}Clefs générées.${NC}"
+  echo "${BLUE}3- SSH.${NC}"
+  chown -R $userdeladebian /home/$userdeladebian/.ssh/ #Attribution du dossier .ssh/ a l'user
+  su -l $userdeladebian -c "ssh-keygen -t rsa" #génération des Clefs
+  echo "${GREEN}Clefs générées.${NC}"
 elif [ $chx_menu = 4 ]; then # test si le numéro 4 est sélectionner.
-  echo -e "${BLUE}4- MariaDB.${NC}"
+  echo "${BLUE}4- MariaDB.${NC}"
   command > /dev/null 2>&1
-  echo -e "${GREEN}---Configuration de MariaDB---${NC}"
+  echo "${GREEN}---Configuration de MariaDB---${NC}"
   yum install mariadb-server #Installe MariaDB
   sudo systemctl start mariadb #Lance le système MariaDB
   sudo systemctl enable mariadb #Active MariaDB a chaque démarrage de la machine
   firewall-cmd --add-port=3306/tcp #Ouverture du port 3306
   firewall-cmd --permanent --add-port=3306/tcp #Ouverture permanente du port 3306
-  mysql -u root -e "create user adminbdd;" #Creation de l'user 'Adminbdd'
-  mysql -u root -e "create database tutoratBot;" #Création de la BDD marcachat
+  mysql -u root "create user adminbdd;" #Creation de l'user 'Adminbdd'
+  mysql -u root "create database tutoratBot;" #Création de la BDD marcachat
   #mysql -u root -p marcachat < marcachatfinal.sql #Import du script SQl dans la base de données Marcachat
   #echo -e "${RED}Avant de CONTINUER !!!!${NC} transferer le fichier 'index.php' et donner la localisation de l'erreur d'accès"
   #read location
-  echo -e "${GREEN}---- Voila MariaDB est configurée ! Félicitations ! A vous la joi des requetes ${RED}SANS CONCATENATION ! "
+  echo "${GREEN}---- Voila MariaDB est configurée ! Félicitations ! A vous la joi des requetes ${RED}SANS CONCATENATION ! "
 elif [ $chx_menu = 5 ]; then # test si le numéro 5 est sélectionner.
-  echo -e "${RED}Tu nous quittes :c${NC}"
+  echo "${RED}Tu nous quittes :c${NC}"
   #reboot
   exit 1
 fi
