b024c51b81a0a9328004f739c8ae68bb805a783c
Kraystone <leo.catifait@epsi.fr>
2020-09-13 16:10:31 +0200

    rmCloneChmod

---
+++ rmCloneChmod.sh
@@ -0,0 +1,15 @@
+RED='\033[0;31m'
+NC='\033[0m'
+
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+  echo -e "${RED}Tu n'es pas en root :'(${NC}"
+  exit 1
+fi
+
+rm -r script-deploiement
+git clone https://github.com/Slinah/script-deploiement.git
+cd /home/leo/script-deploiement
+chmod +x /home/leo/script-deploiement/BashTutoratBot.sh
+exit 0
