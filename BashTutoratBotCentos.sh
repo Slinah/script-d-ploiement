33f6d51de4b06d746e12d6e296cc6faa00a2fdf8
Kraystone <leo.catifait@epsi.fr>
2020-09-19 12:49:18 +0200

    echo -e

--- BashTutoratBotCentos.sh
+++ BashTutoratBotCentos.sh
@@ -1,5 +1,6 @@
   #!/bin/bash
  #command to execute it on debian chmod +x .sh
+ #echo -e for colors in Centos
 
 RED='\031[0;47m'
 BLUE='\033[0;34m'
@@ -9,18 +10,18 @@ userdeladebian = "leo"
 echo $userdeladebian
 
 if [ "$(id -u)" != 0 ]; then
-  echo "${RED}Tu n'es pas en root :'(${NC}"
+  echo -e "${RED}Tu n'es pas en root :'(${NC}"
   exit 2
 fi
 while :
 do
-echo "
+echo -e "
 ${GREEN}---Menu du Script---${NC}
 
-${BLUE}1- Update/Upgrade & installation des paquets.${NC}
-${BLUE}2- Configuration de la BDD.${NC}
-${BLUE}3- L'api ruby, et l'api discord.${NC}
-${BLUE}4- Quitter le script et reboot de la machine --->[].${NC}
+${BLUE} 1- Update/Upgrade & installation des paquets.${NC}
+${BLUE} 2- Configuration de la BDD.${NC}
+${BLUE} 3- L'api ruby, et l'api discord.${NC}
+${BLUE} 4- Quitter le script et reboot de la machine --->[].${NC}
 
 ${GREEN}--------------------${NC}
 "
@@ -29,19 +30,19 @@ read chx_menu
 stty echo
 
 if [ $chx_menu = 1 ]; then # test si le numéro 1 est sélectionner.
-  echo "${BLUE}1- Update/Upgrade & installation des paquets.${NC}" 
+  echo -e "${BLUE}1- Update/Upgrade & installation des paquets.${NC}" 
   yum -y update
   yum -y upgrade
   #Installation du paquet MariaDB
-  echo "${GREEN}Update/Upgrade effectués${NC}"
+  echo -e "${GREEN}Update/Upgrade effectués${NC}"
   yum install wget -y 
-  echo "${GREEN}Paquet Wget installé${NC}"
-  echo "${GREEN}Installation de MariaDB${NC}"
+  echo -e "${GREEN}Paquet Wget installé${NC}"
+  echo -e "${GREEN}Installation de MariaDB${NC}"
   #Installation du paquet MariaDB
   yum install mariadb-server 
-  echo "${GREEN}Paquet MariaDB installé.${NC}"
+  echo -e "${GREEN}Paquet MariaDB installé.${NC}"
 elif [ $chx_menu = 2 ]; then # test si le numéro 1 est sélectionner.
-  echo "${GREEN}---Configuration de MariaDB---${NC}"
+  echo -e "${GREEN}---Configuration de MariaDB---${NC}"
   systemctl start mariadb.service #Lance le système MariaDB
   systemctl enable mariadb.service #Active MariaDB a chaque démarrage de la machine
   firewall-cmd --add-port=3306/tcp 
@@ -50,31 +51,31 @@ elif [ $chx_menu = 2 ]; then # test si le numéro 1 est sélectionner.
   mysql -u root -e "create database tutorat;" #Création de la BDD tutorat
   mysql -u root -p tutorat < tutoratrefonte.sql #Import du script SQl dans la base de données tutoratrefonte.sql
   mysql -u root -e "GRANT ALL privileges ON tutorat.* TO adminbot;" #Ajouts des privilèges de adminbot
-  echo "${GREEN}---- Voila MariaDB est configurée ! Félicitations !"
+  echo -e "${GREEN}---- Voila MariaDB est configurée ! Félicitations !"
 elif [ $chx_menu = 3 ]; then # test si le numéro 3 est sélectionner.
   yum install ruby-full -y # installation de ruby
-  echo "${GREEN}Ruby installe.${NC}"
+  echo -e "${GREEN}Ruby installe.${NC}"
   git clone https://github.com/Slinah/api-refonte-tutorat.git # on clone l'api de notre bot
-  echo "${GREEN}L'API ruby a été clone.${NC}"
+  echo -e "${GREEN}L'API ruby a été clone.${NC}"
   gem install bundler
   su -l leo -c " bundle install"
   apt install libssl-dev libffi-dev libsqlite3-dev zlib1g-dev gcc g++ make
   wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tgz #modifier et mise a jour de python 3.8
-  echo "${GREEN}Python est installé.${NC}"
+  echo -e "${GREEN}Python est installé.${NC}"
   tar xzvf Python-3.8.5.tgz
   cd Python-3.8.5/
   ./configure
   make
   make install
-  echo "${GREEN}Python OK.${NC}"
+  echo -e "${GREEN}Python OK.${NC}"
   wget https://bootstrap.pypa.io/get-pip.py #install pip
   yum upgrade get-pip.py #upgrade pip a test
   python3 -m pip install -U discord.py[voice] #s'intall parfaitement
-  echo "${GREEN}L'API Discord.py a été cloné.${NC}"
+  echo -e "${GREEN}L'API Discord.py a été cloné.${NC}"
   mkdir bot
   cd bot
   git clone https://github.com/Slinah/stage_refonte_bot.git
-  echo "${GREEN}Le bot a été cloné.${NC}"
+  echo -e "${GREEN}Le bot a été cloné.${NC}"
   sudo systemctl start python3 bot.py #Lance le système du bot
   sudo systemctl enable python3 bot.py #Active le bot a chaque démarrage de la machine
   cd
